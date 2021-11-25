8b91d85e174a56bde880895e4b346749e128d53a
slowriding <48739803+slowriding@users.noreply.github.com>
2021-11-25 18:47:10 -0500

    i wanna make it great

--- Src/DLL.cpp
+++ Src/DLL.cpp
@@ -1,5 +1,5 @@
 #include "Common/Abstract.h"
-
+//exemple of a dll function
 void addSrcto(Abstract *AB)
 {
     void *ptr = AB_ReadNextParam(AB);
@@ -18,11 +18,14 @@ void rmSrcfrom(Abstract *AB)
 {
     cout << "!Hello From DLL" << endl;
 };
+//                                                       |-------------as well as DLL_EXIT----_------------|
+//when your dll is loaded this is the first function run it take a Abstract* (as context) and output nothing
 DLL_INIT(Abstract *This)
 {
     AB_Insert(This, "+src", &addSrcto);
     AB_Insert(This, "-src", &rmSrcfrom);
 }
+//executed when dll is destroy (consider deleting your entry from Abstract)
 DLL_EXIT(Abstract *This){
     cout<<"DLL Kill"<<endl; 
 }
